243ca65828a8df0b9a5e98ba7b65bab86929207f
Rabatesky <Rabatesky@yandex.ru>
2024-06-19 12:49:38 +0300

    new_dag_sync

---
+++ dags/dag2.py
@@ -0,0 +1,27 @@
+from datetime import datetime
+from datetime import timedelta
+from airflow.models import DAG
+from airflow.operators.bash import BashOperator
+
+
+
+default_args = {
+    'owner': 'home_pc',
+    'depends_on_past': False,
+    'start_date': datetime(2024, 6, 19),
+    'retries': 0,
+    'max_active_runs': 1,
+    'retry_delay': timedelta(minutes=0.1)
+}
+
+dag = DAG('sync_git', default_args=default_args, schedule_interval='* * * * *',
+          catchup=False, max_active_runs=1, max_active_tasks=3, tags=["idiot"])
+
+start_sync = BashOperator(
+    task_id = 'start_sync',
+    bash_command='python3 /airflow/scripts/dag2/git_sync_airflow.py.py',
+    dag=dag
+)
+
+start_sync
+
