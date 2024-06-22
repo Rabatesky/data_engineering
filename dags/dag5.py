9c7a3acc3933cb6f88205b5578c706c16930e6e7
Rabatesky <Rabatesky@yandex.ru>
2024-06-22 16:41:25 +0300

    reload_dag5_2

--- dags/dag5.py
+++ dags/dag5.py
@@ -26,7 +26,7 @@ def get_new_table_postgres():
     pg_hook = PostgresHook('1_my_postgres_test')
     con = pg_hook.get_conn()
     data = pd.read_sql_query("Select * from california.california_housing LIMIT 10", con)
-    logging.info(data)
+    logging.info(pg_hook.get_conn)
 
 
 test_connect = PythonOperator(
